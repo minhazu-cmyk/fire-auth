3a2ba6b128a4c4af90ec3ad415e27b47bdf0e7a1
minhazu-cmyk <minhajabedin341@gmail.com>
2020-09-10 10:48:07 +0000

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,109 @@
-import React from 'react';
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import * as firebase from "firebase/app";
+import "firebase/auth";
+// firebase.initializeApp(firebaseCon)
+import firebaseConfig from "./firebase.config";
+
+firebase.initializeApp(firebaseConfig);
 
 function App() {
+  const [user,setUser] = useState({
+
+    isSignedIn:false,
+    name:'',
+    email:'',
+    pass: "",
+
+  })
+  const provider = new firebase.auth.GoogleAuthProvider();
+  const handleClick =()=>{
+    firebase.auth().signInWithPopup(provider)
+   .then(res=>{
+     const{displayName,email,emailVerified}= res.user;
+     const signUser={
+       isSignedIn:true,
+       name:displayName,
+       email:email,
+     }
+     setUser(signUser);
+     console.log(displayName,email,emailVerified)
+   })
+  .catch(error=>{
+    console.log(error)
+    console.log(error.message)
+  })
+
+  }
+  const handleOut=()=>{
+    firebase.auth().signOut()
+    .then(res=>{
+    const isSignOut={
+      isSignedIn: false,
+      name:"",
+      email:"",
+     
+    }
+    setUser(isSignOut)
+
+  })
+   .catch(error=>{
+     
+   })
+  }
+  const handleBlur=(event)=>{
+console.log(event.target.name,event.target.value)
+let formValid= true;
+if(event.target.name==="email"){
+ formValid= /\S+@\S+\.\S+/.test(event.target.value);
+
+}
+if(event.target.name==="pass"){
+  const isPassValid = event.target.value.length>6;
+const passwordValue= /\d{1}/.test(event.target.value)
+
+  formValid = isPassValid && passwordValue;
+}
+if(formValid){
+const newInfo= {...user};
+newInfo[event.target.name]= event.target.value;
+setUser(newInfo)
+
+}
+
+  }
+  const handleSubmit=(event)=>{
+console.log(user.email,user.pass)
+if(user.email && user.pass){
+
+console.log("submitted")
+}
+event.preventDefault();
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div >
+      {
+        user.isSignedIn ?  <button onClick={handleOut}> sign out</button> : <button onClick={handleClick}> sign in</button>
+      }
+      {
+        user.isSignedIn && <div> <p>welcome {user.name} </p>
+        <p> email:{user.email} </p>
+      
+        </div>
+}
+       <h2> my own authentication </h2>
+       
+       <form onSubmit={handleSubmit}>
+         <input type="text" name="name"onBlur={handleBlur} placeholder="ur name"/>
+         <br/>
+      <input type="email" name="email" onBlur={handleBlur} id="" placeholder="write your email here" required/>
+      <br/>
+        <input type="password" onBlur={handleBlur} name="pass" id="" placeholder="pass here" required/>
+        <br/>
+        <input type="submit" value="submit"/>
+        </form>
+
     </div>
   );
 }
---
+++ src/firebase.config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyD7YmseNjLRoZj6MibaKq5oW_5b6-HF-aI",
+  authDomain: "ema-jhon-simple-acee0.firebaseapp.com",
+  databaseURL: "https://ema-jhon-simple-acee0.firebaseio.com",
+  projectId: "ema-jhon-simple-acee0",
+  storageBucket: "ema-jhon-simple-acee0.appspot.com",
+  messagingSenderId: "153295057985",
+  appId: "1:153295057985:web:58051cbb6e4bf5acf782c0"
+};
+export default firebaseConfig;
